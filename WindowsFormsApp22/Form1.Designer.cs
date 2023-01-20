a42e69f775370c725cb5ec068fa4e25a2487cd70
RAMP <54576715+vlad424@users.noreply.github.com>
2023-01-20 11:49:33 +0700

    1

---
+++ WindowsFormsApp22/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace WindowsFormsApp22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Физические данные",
+            "Биография",
+            "Документы",
+            "Хобби",
+            "Мультимедиа"});
+            this.comboBox1.Location = new System.Drawing.Point(4, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 93);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Скрыть";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 117);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Показать";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 93);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Скрыть/Показать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 0);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(14, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Выключить приложение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Используется:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(316, 381);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0 min 0 sec";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 151);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(150, 146);
+            this.tabControl1.TabIndex = 14;
+            this.tabControl1.Visible = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(142, 120);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Первая";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(142, 120);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Вторая";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(149, 37);
+            this.panel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 406);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Сводка";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp22/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp22
+{
+    public partial class Form1 : Form
+    {
+        int s = 0;
+        int m = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Icon = SystemIcons.Question;// Изменение иконки приложения
+        }
+
+        private void HideE() //Показать картинки 
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            label3.Visible = true;
+            Off();
+        }
+        private void UnHide() //Скрыть каритнки
+        {
+            if(radioButton2.Checked == true)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+            }
+
+            label3.Visible = false;
+            On();
+        }
+        private void Off() // невидимые точки
+        {
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+        }
+        private void On() // Видимые точки
+        {
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+        }
+
+        private void AData() //Физ данные
+        {
+            HideE();
+            label3.Text = Properties.Resources.aboutData;
+            checkBox1.Visible = false;
+            tabControl1.Visible = false;
+        }
+        private void Bio() // Биография
+        {
+            HideE();
+            label3.Text = Properties.Resources.bio;
+            checkBox1.Visible = false;
+            tabControl1.Visible = false;
+        }
+        private void Docs() //Документы
+        {
+            tabControl1.Visible = true;
+            if(checkBox1.Checked == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+            }
+            label3.Visible = false;
+            checkBox1.Visible = true;
+            tabControl1.Visible = true;
+
+            pictureBox1.Image = Properties.Resources._1;
+            pictureBox2.Image = Properties.Resources._2;
+        }
+        private void Hobby()//Хобби
+        {
+            HideE();
+            label3.Text = Properties.Resources.hobby;
+            checkBox1.Visible = false;
+            tabControl1.Visible = false;
+        }
+        private void MultiMedia() // Мультимедиа
+        {
+            tabControl1.Visible = true;
+            HideE();
+            UnHide();
+            pictureBox1.Image = Properties.Resources._3;
+            pictureBox2.Image = Properties.Resources._4;
+            checkBox1.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sI = comboBox1.SelectedIndex;
+
+            if(sI == 0) // физ данные
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[sI]);
+                AData();
+            }
+            if (sI == 1) // биография
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[sI]);
+                Bio();
+            }
+            if (sI == 2) // документы
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[sI]);
+                Docs();
+            }
+            if (sI == 3) // хобби
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[sI]);
+                Hobby();
+            }
+            if (sI == 4) // мультимедиа
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[sI]);
+                MultiMedia();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked == true) //Невидимые картинки
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true) //Видимы картинки
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true) // невидимы
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;// видимы
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "1") // физ данные
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[0]);
+                AData();
+                textBox1.Text = "";
+            }
+            if (textBox1.Text == "2") // биография
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[1]);
+                Bio();
+                textBox1.Text = "";
+            }
+            if (textBox1.Text == "3") // документы
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[2]);
+                Docs();
+                textBox1.Text = "";
+            }
+            if (textBox1.Text == "4") // хобби
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[3]);
+                Hobby();
+                textBox1.Text = "";
+            }
+            if (textBox1.Text == "5") // мультимедиа
+            {
+                label1.Text = Convert.ToString(comboBox1.Items[4]);
+                MultiMedia();
+                textBox1.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Выключение приложения
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // таймер
+        {
+            s++;
+            
+            if(s == 58)
+            {
+                m++;
+                s = 0;
+            }
+
+            label5.Text = m + " min " + s + " sec";
+        }
+    }
+}
